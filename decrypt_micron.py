7c696c7145c03d8da3b579a5e08f3bea0b9be498
J0ki <joki0z@web.de>
2016-06-03 01:23:56 +0200

    add decrypt script with hardcoded masterkey

---
+++ decrypt_micron.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+#TODO: optional: remove key exchange from stream
+
+import sys
+
+def decrypt(data, key):
+  return bytes([d ^ key for d in data])
+
+input = sys.stdin.buffer
+output = sys.stdout.buffer
+
+MASTERKEY = 0x0b
+
+key = 0x00
+data = bytes()
+eos = False
+while True:
+  if eos and not data:
+    break
+  i = data.find(0xab)
+  if i == -1:
+    output.write(decrypt(data, key))
+    data = input.read(1024)
+    eos = len(data) < 1024
+    continue
+  output.write(decrypt(data[:i], key))
+  more_data = input.read(12)
+  eos = len(more_data) < 12
+  data = data[i:] + more_data
+  if len(data) < 12:
+    output.write(decrypt(data, key))
+    break
+  if data[:5] == bytes([0xab, 0xbc, 0xcd, 0xde, 0xea]):
+    key = data[12] ^ MASTERKEY
+    output.write(data[:11])
+    data = data[11:]
+  else:
+    output.write(decrypt(data[:1], key))
+    data = data[1:]
+
+
+#~ key = int(sys.argv[2], 16)
+
+#~ for line in open(sys.argv[1]):
+  #~ line = line.split("\n")[0]
+  #~ if line:
+    #~ if line.startswith("Host   --> "):
+      #~ print("Host   --> ")
+      #~ line = line[11:]
+    #~ if line.startswith("Device --> "):
+      #~ print("Device --> ")
+      #~ line = line[11:]
+    #~ if not line.startswith("==>"):
+      #~ print("  " + line)
+      #~ bytes = line.split(" ")
+      #~ bytes = [int(x,16) for x in bytes]
+      #~ bytes = [x ^ key for x in bytes]
+      #~ bytes = ["{:02x}".format(x, "x") for x in bytes]
+      #~ line = "~>" + " ".join(bytes)
+    #~ print(line)
