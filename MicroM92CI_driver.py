f054c415a4c31b077962a8ee3a3eed9fac26bdeb
J0ki <joki0z@web.de>
2016-06-14 16:50:54 +0200

    make private module fields private with _underscore

--- MicroM92CI_driver.py
+++ MicroM92CI_driver.py
@@ -5,76 +5,77 @@ import sys
 import time
 import json
 
-def blob_to_hex(bytestring):
+
+_MASTERKEY = bytes((0x0e, 0x0b))
+
+_CMD_PREFIX = bytes([0x8e])
+_CMD_DISPLAY_PREFIX      = _CMD_PREFIX + bytes([0x41])
+_CMD_STANDBY_PREFIX      = _CMD_PREFIX + bytes([0x67])
+_CMD_CONFIRM_KEYEXCHANGE = _CMD_PREFIX + bytes([0x08])
+
+
+def _blob_to_hex(bytestring):
   return " ".join("{:02x}".format(c) for c in bytestring)
 
-def hex_list_from_int(number):
+def _hex_list_from_int(number):
   h, l  = divmod(number, 16)
   hex_list = [l]
   if 0 != h:
-    hex_list = hex_list_from_int(h) + hex_list
+    hex_list = _hex_list_from_int(h) + hex_list
   return hex_list
 
-def hexdigit(i):
+def _hexdigit(i):
   if i < 10:
     return 16*i + 3 # {0x03..0x93}
   if i < 16:
     return 16*(i-9) + 2 #{0x12..0x62}
   return 0
 
-def hex_to_display(number):
-  displaystring = bytes([ hexdigit(i) for i in hex_list_from_int(number) ])
-  return cmd_display_raw(displaystring)
+def _hex_to_display(number):
+  displaystring = bytes([ _hexdigit(i) for i in _hex_list_from_int(number) ])
+  return _cmd_display_raw(displaystring)
 
-def hex_to_blob(asciistring):
-  return bytes([int(x,16) for x in asciistring.split(" ")])
+#~ def _hex_to_blob(asciistring):
+  #~ return bytes([int(x,16) for x in asciistring.split(" ")])
 
-def swapnibbles(x):
+def _swapnibbles(x):
   return x<<4 & 0xf0 | x>>4 & 0x0f
 
-def ascii_to_display(string):
+def _ascii_to_display(string):
   displaystring = b''
   for c in string:
     if c.isdigit():
-      displaystring += bytes([swapnibbles(ord(c))])
+      displaystring += bytes([_swapnibbles(ord(c))])
     else:
       displaystring += ord(c)
   return displaystring
 
-def cmd_display(string):
-  cmd = CMD_DISPLAY_PREFIX
+def _cmd_display(string):
+  cmd = _CMD_DISPLAY_PREFIX
   if len(string) > 16:
     return b''
-  cmd += ascii_to_display(string)
+  cmd += _ascii_to_display(string)
   cmd = cmd.ljust(18, b'\0')
   return cmd
 
-def cmd_display_raw(bytestring):
-  cmd = CMD_DISPLAY_PREFIX
+def _cmd_display_raw(bytestring):
+  cmd = _CMD_DISPLAY_PREFIX
   if len(bytestring) > 16:
     return b''
   cmd += bytestring
   cmd = cmd.ljust(18, b'\0')
   return cmd
 
-def format_standby_time(t):
+def _format_standby_time(t):
   h1, h2 = divmod(t.tm_hour, 10)
   m1, m2 = divmod(t.tm_min, 10)
   s1, s2 = divmod(t.tm_sec, 10)
   cmd = ''.join(map(str,[h1,h2,m1,m2,s1,s2]))
   #~ print(cmd)
-  return ascii_to_display(cmd).ljust(9, b'\0')
+  return _ascii_to_display(cmd).ljust(9, b'\0')
 
-def cmd_standby(t):
-  return CMD_STANDBY_PREFIX.ljust(9, b'\0') + format_standby_time(t)
-
-
-MASTERKEY = bytes((0x0e, 0x0b))
-
-CMD_PREFIX = bytes([0x8e])
-CMD_DISPLAY_PREFIX      = CMD_PREFIX + bytes([0x41])
-CMD_STANDBY_PREFIX      = CMD_PREFIX + bytes([0x67])
-CMD_CONFIRM_KEYEXCHANGE = CMD_PREFIX + bytes([0x08])
+def _cmd_standby(t):
+  return _CMD_STANDBY_PREFIX.ljust(9, b'\0') + _format_standby_time(t)
 
 class M92CI_IR:
 
@@ -88,7 +89,7 @@ class M92CI_IR:
     data = self.ser.read(n)
     if data:
       if self.verbose:
-        print("<== " + blob_to_hex(data))
+        print("<== " + _blob_to_hex(data))
     return data
 
   def readdata(self, n):
@@ -96,17 +97,17 @@ class M92CI_IR:
     if data:
       data = self.crypt_list_8bit(data)
       if self.verbose:
-        print("<== " + blob_to_hex(data))
+        print("<== " + _blob_to_hex(data))
     return data
 
   def senddata_clear(self, packet):
     if self.verbose:
-      print("--> " + blob_to_hex(packet))
+      print("--> " + _blob_to_hex(packet))
     self.ser.write(packet)
 
   def senddata(self, packet):
     if self.verbose:
-      print("--> " + blob_to_hex(packet))
+      print("--> " + _blob_to_hex(packet))
     packet = self.crypt_list_8bit(packet)
     self.ser.write(packet)
 
@@ -141,18 +142,18 @@ class M92CI_IR:
     data = self.readdata_clear(6)
     if len(data) < 6:
       raise ConnectionError("bad response:("+str(len(data))+") "+ str(data))
-    self.senddata(MASTERKEY)
+    self.senddata(_MASTERKEY)
 
-    self.senddata(CMD_CONFIRM_KEYEXCHANGE)
+    self.senddata(_CMD_CONFIRM_KEYEXCHANGE)
     self.readdata(3)
     if len(data) < 3:
       raise ConnectionError("bad response:("+str(len(data))+") "+ str(data))
 
   def standby(self, standby_time=None):
     if standby_time:
-      cmd = cmd_standby(standby_time)
+      cmd = _cmd_standby(standby_time)
     else:
-      cmd = cmd_standby(time.localtime())
+      cmd = _cmd_standby(time.localtime())
     self.senddata(cmd)
 
   def generate_keymap(self):
@@ -161,26 +162,26 @@ class M92CI_IR:
       data = self.readdata(2)
       if len(data) < 2:
         continue
-      if data == CMD_STANDBY_PREFIX:
+      if data == _CMD_STANDBY_PREFIX:
         break
       keycode = data[1]
       remote_control_key = input(str(keycode)+": ")
       keymap[keycode] = remote_control_key
-      self.senddata(hex_to_display(data[0]<<8 | data[1]))
+      self.senddata(_hex_to_display(data[0]<<8 | data[1]))
     return keymap
 
   def display_all_decimals(self):
     for i in range(0, 10000):
       i = str(i).rjust(4, "0")
-      self.senddata(cmd_display(i))
+      self.senddata(_cmd_display(i))
 
   def display_raw_bytes(self):
     for i in range(0, 0x100):
-      i = swapnibbles(i)
-      #~ cmd = cmd_display_raw(bytes((i,(i+0x10)&0xff,(i+0x20)&0xff,(i+0x30)&0xff)))
+      i = _swapnibbles(i)
+      #~ cmd = _cmd_display_raw(bytes((i,(i+0x10)&0xff,(i+0x20)&0xff,(i+0x30)&0xff)))
       time.sleep(0.5)
 
-      cmd = hex_to_display(i)
+      cmd = _hex_to_display(i)
       time.sleep(.5)
       self.senddata(cmd)
 
